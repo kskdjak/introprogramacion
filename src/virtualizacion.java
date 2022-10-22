2c2fe818b274e4442b41808894503baabe729d9d
Lenin Cuenca <loja_2002eceuador@hotmail.com>
2022-10-21 19:48:26 -0500

    creacion de nueva clases llamda virtualizacion2

--- src/virtualizacion.java
+++ src/virtualizacion.java
@@ -1,6 +1,10 @@
 public class virtualizacion {
     public static void main(String[] args) {
         System.out.println("TEMAS DE PROYECTOS EXPLICADOS EN CENTROS DE CONVENCIONES");
+        System.out.println("Filtros de realidad aumentada");
+        System.out.println("implentacion de un videojuego para la juventud idente");
+        System.out.println("implementacion de laboratoria multiusuario para practicas en un set de TV");
+        System.out.println("Desarrolo  de videojuegos en el marco de la materia juegos de la carrera computacion ");
 
 
     }
